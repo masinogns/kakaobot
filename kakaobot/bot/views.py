bdc409f0d30050c7c4d7d9218a5382cabfa1d5f8
b.kihoon <masinogns@gmail.com>
2017-05-17 21:41:28 +0900

    first

---
+++ kakaobot/bot/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import JsonResponse
+
+def keyboard(request):
+
+    return JsonResponse({
+        'type' : 'buttons',
+        'buttons' : ['1', '2']
+    })
