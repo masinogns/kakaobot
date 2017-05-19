1b390decc9a17a2fa244b3b88dac15799a10684a
b.kihoon <masinogns@gmail.com>
2017-05-20 00:41:11 +0900

    change views.py git add .

--- kakaobot/bot/views.py
+++ kakaobot/bot/views.py
@@ -8,10 +8,12 @@ import json, datetime
 
 def keyboard(request):
 
-    return JsonResponse({
-        'type' : 'buttons',
-        'buttons' : ['학식', '정문', '후문']
-    })
+    return JsonResponse(
+    {
+    "type" : "buttons",
+    "buttons" : [u"오늘의 메뉴", u"내일의 메뉴", u"이번주 메뉴"]
+    }
+    )
 
 @csrf_exempt
 def answer(request):
@@ -21,13 +23,31 @@ def answer(request):
 
     today_date = datetime.date.today().strftime("%m월 %d일")
 
-    return JsonResponse({
-            'message': {
-                'text': today_date + '의 ' + cafeteria_name + ' 중식 메뉴입니다.'
+    return JsonResponse(
+    {
+        {
+            "message":{
+                "text" : "제대로 알려주세요!\n어떤 맛있는 메뉴가 기다리고 있을까요?"
             },
-            'keyboard': {
-                'type': 'buttons',
-                'buttons' : ['학식', '정문', '후문']
+            "keyboard" : {
+                "type" : "buttons",
+                "buttons" : [
+                    u"오늘의 메뉴",
+                    u"내일의 메뉴",
+                    u"이번주 메뉴"
+                ]
             }
+        },
+        {
+            "message":{
+                "text" : "=오늘의 메뉴=\n밥\n된장국\n돈까스\n뾰로롱"
+            } # keyboard는 모든 메세지에 붙어있다. 코드 길이상 생략했다.
+        },
+        {
+            "message":{
+                "text" : "=내일의 메뉴=\n연어덮밥\n먹고싶다\n돈까스도\n먹고싶다"
+            }
+        }
+    }
 
-        })
+    )
